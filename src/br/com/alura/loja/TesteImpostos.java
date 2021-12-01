3eadf465ce28c527956cc2b31ff7aa3bde9b0816
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-12-01 10:56:44 -0300

    Alterando o nome para TesteImpostos

--- src/br/com/alura/loja/TesteImpostos.java
+++ src/br/com/alura/loja/TesteImpostos.java
@@ -8,9 +8,9 @@ import br.com.alura.loja.imposto.ISS;
 import br.com.alura.loja.imposto.Imposto;
 import br.com.alura.loja.orcamento.Orcamento;
 
-public class Teste {
+public class TesteImpostos {
     public static void main(String[] args) {
-        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
         CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
 
         System.out.println(calculadora.calcular(orcamento, new ICMS()));
