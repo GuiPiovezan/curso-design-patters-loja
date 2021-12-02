5328a7dc04a4a3bea5445fc59d30507ae98c2f32
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-12-02 09:07:04 -0300

    Padrão de projeto command, onde há classes que representam a criação no banco de dados com as devidas injeções de dependências: services, repository,etc.

--- src/br/com/alura/loja/TestePedido.java
+++ src/br/com/alura/loja/TestePedido.java
@@ -4,15 +4,18 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.pedido.GeraPedido;
+import br.com.alura.loja.pedido.GeraPedidoHandler;
 import br.com.alura.loja.pedido.Pedido;
 
 public class TestePedido {
     public static void main(String[] args) {
-        Orcamento orcamento = new Orcamento(new BigDecimal("100"), 4);
+        String cliente = args[0];
+        int quantidadeItens = Integer.parseInt(args[2]);
+        BigDecimal valorOrcamento = new BigDecimal(args[1]);
 
-        String cliente = "Guilherme Piovezan";
-        var data = LocalDateTime.now();
-
-        Pedido pedido = new Pedido(cliente, data, orcamento);
+        GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
+        GeraPedidoHandler handler = new GeraPedidoHandler();
+        handler.execute(gerador);
     }
 }
---
+++ src/br/com/alura/loja/pedido/GeraPedidoHandler.java
@@ -0,0 +1,17 @@
+package br.com.alura.loja.pedido;
+
+import java.time.LocalDateTime;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class GeraPedidoHandler {
+    
+
+    // injeção de depêndencias: repository, services, etc. 
+
+    public void execute(GeraPedido dados){
+        Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
+
+        Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
+    }
+}
